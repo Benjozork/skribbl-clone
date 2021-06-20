2e24693d9e71292cfdece4320c08e10de07166b0
ErickSharp <titaninmc@gmail.com>
2021-06-20 03:20:10 -0500

    feat: add theme-based styling to login page and toolbar

--- client/src/GameBoard/ToolBox.tsx
+++ client/src/GameBoard/ToolBox.tsx
@@ -1,13 +1,43 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
 import React, { useState, FC } from 'react';
+import { IconSpeakerphone, IconArrowBackUp, IconArrowForwardUp, IconEraser, IconBucket, IconTrash, IconThumbUp, IconThumbDown } from '@tabler/icons';
 import { DrawingCanvasControllerUsage } from './DrawingCanvas';
+import { themeColors } from '../index';
 
 export type ToolBoxProps = {
     onUseController: (usage: DrawingCanvasControllerUsage) => void,
 }
 
+type buttonProps = {
+    onClick: any,
+    className?: string
+}
+
+type colorSelectionProps = { hexColor: string };
+
+const Button: FC<buttonProps> = ({ onClick, className, children }) => (
+    <div
+        className={`${className} flex items-center hover:border-current border-2 border-transparent transition duration-200 py-2 justify-center w-full rounded-sm cursor-pointer select-none`}
+        style={{ background: themeColors.STANDARD, color: themeColors.ACCENT }}
+        onClick={onClick}
+    >
+        {children}
+    </div>
+);
+
+const Divider = () => (
+    <div className="w-full h-1 rounded-lg" style={{ background: themeColors.ACCENT }} />
+);
+
+const ColorPanel: FC = ({ children }) => (
+    <div className="grid grid-cols-4 w-full py-2 justify-items-center gap-y-1" style={{ background: themeColors.STANDARD }}>
+        {children}
+    </div>
+);
+
 export const ToolBox: FC<ToolBoxProps> = ({ onUseController }) => {
-    const [selectedTool] = useState(0);
+    // const [selectedTool] = useState(0);
+    const [selectedColor, setSelectedColor] = useState('#FFFFFF');
 
     const handleClear = () => {
         onUseController((canvas, context) => {
@@ -19,23 +49,69 @@ export const ToolBox: FC<ToolBoxProps> = ({ onUseController }) => {
         onUseController((_, context) => {
             context.fillStyle = color;
         });
+        setSelectedColor(color);
     };
 
-    return (
-        <div className="text-2xl flex flex-row justify-center space-x-10">
-            <span>
-                selected:
-                {selectedTool}
-            </span>
-            <span className="cursor-pointer">pen</span>
-            <span className="cursor-pointer">large pen thing</span>
-            <span className="cursor-pointer">fill</span>
-            <span className="cursor-pointer" onClick={handleClear}>clear</span>
+    const ColorSelection = ({ hexColor }: colorSelectionProps) => (
+        <div
+            className={`${hexColor === selectedColor ? 'border-current/40' : 'border-transparent'} border-2 w-6 h-6 rounded-full cursor-pointer`}
+            style={{ background: hexColor, color: themeColors.ACCENT }}
+            onClick={() => handleChooseColor(hexColor)}
+        />
+    );
 
-            <span className="w-10 h-10 bg-red-500" onClick={() => handleChooseColor('red')} />
-            <span className="w-10 h-10 bg-yellow-500" onClick={() => handleChooseColor('yellow')} />
-            <span className="w-10 h-10 bg-green-500" onClick={() => handleChooseColor('green')} />
-            <span className="w-10 h-10 bg-blue-500" onClick={() => handleChooseColor('blue')} />
+    return (
+        <div className="text-2xl w-32 mx-10 flex flex-col justify-center space-y-3">
+            <Button onClick={() => console.log()}>
+                <IconSpeakerphone size={32} />
+            </Button>
+            <div className="space-y-1.5">
+                <Divider />
+                <Divider />
+            </div>
+            <div className="flex flex-row gap-x-3">
+                <Button onClick={() => console.log()}>
+                    <IconArrowBackUp size={32} />
+                </Button>
+                <Button onClick={() => console.log()}>
+                    <IconArrowForwardUp size={32} />
+                </Button>
+            </div>
+            <Divider />
+            <ColorPanel>
+                <ColorSelection hexColor="#FFFFFF" />
+                <ColorSelection hexColor="#FF0000" />
+                <ColorSelection hexColor="#FFF400" />
+                <ColorSelection hexColor="#0045FF" />
+                <ColorSelection hexColor="#000000" />
+                <ColorSelection hexColor="#FF8100" />
+                <ColorSelection hexColor="#22FF00" />
+                <ColorSelection hexColor="#00FFFF" />
+            </ColorPanel>
+            <Divider />
+            <div className="grid grid-cols-2 gap-3">
+                <Button onClick={() => console.log()}>
+                    <IconEraser size={32} />
+                </Button>
+                <Button onClick={() => console.log()}>
+                    <IconBucket size={32} />
+                </Button>
+                <Button className="col-span-full" onClick={handleClear}>
+                    <IconTrash size={32} />
+                </Button>
+            </div>
+            <div className="space-y-1.5">
+                <Divider />
+                <Divider />
+            </div>
+            <div className="flex flex-row gap-x-3">
+                <Button onClick={() => console.log()}>
+                    <IconThumbUp size={32} />
+                </Button>
+                <Button onClick={() => console.log()}>
+                    <IconThumbDown size={32} />
+                </Button>
+            </div>
         </div>
     );
 };
